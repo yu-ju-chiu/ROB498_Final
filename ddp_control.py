6838c89107525274d9f6e9c4f9fd9a248c71224b
billy8874 <biliyy224432@gmail.com>
2023-04-20 17:57:40 -0400

    Added DDP

---
+++ ddp_control.py
@@ -0,0 +1,130 @@
+import torch
+
+class DDPController(object):
+
+    def __init__(self, env, horizon):
+        """
+        :param env: Simulation environment. Must have an action_space and a state_space.
+        :param horizon: <int> Number of control steps into the future
+        """
+        self.env = env
+        self.T = horizon
+        self.action_size = env.action_space.shape[-1]
+        self.state_size = env.state_space.shape[-1]
+        self.goal_state = torch.zeros(self.state_size)  # This is just a container for later use
+        self.Q = torch.diag(torch.tensor([5.0, 5.0, 5.0, 0.1, 0.1, 0.1]))
+        self.U = torch.zeros((self.T, self.action_size)) # nominal action sequence (T, action_size)
+        self.u_init = torch.zeros(self.action_size)
+        self.X = torch.zeros((self.T, self.state_size))
+        self.x_init = torch.zeros(self.state_size)
+        self.k = torch.zeros((self.T, self.action_size))
+        self.K = torch.zeros((self.T, self.action_size, self.state_size))
+
+    def reset(self):
+        self.U = torch.zeros((self.T, self.action_size))# nominal action sequence (T, action_size)
+
+    def command(self, state):
+        """
+        Run a DDP step and return the optimal action.
+        :param state: torch tensor of shape (state_size,)
+        :return: action: torch tensor of shape (1,)
+        """
+        self.X = self._rollout_dynamics(state, actions=self.U)
+        trajectory_cost = self._compute_trajectory_cost(self.X)
+        self._backward_pass()
+        self._foward_pass()
+        # select optimal action
+        action = self.U[0]
+        # final update nominal trajectory
+        self.U = torch.roll(self.U, -1, dims=0) # move u_t to u_{t-1}
+        self.U[-1] = self.u_init # Initialize new end action
+        self.X = torch.roll(self.X, -1, dims=0) # move x_t to x_{t-1}
+        self.X[-1] = self.x_init # Initialize new end state
+        return action
+
+    def _rollout_dynamics(self, state_0, actions):
+        """
+        Roll out the environment dynamics from state_0 and taking the control actions given by actions
+        :param state_0: torch tensor of shape (state_size,)
+        :param actions: torch tensor of shape (T, action_size)
+        :return:
+         * trajectory: torch tensor of shape (T, state_size) containing the states along the trajectories given by
+                       starting at state_0 and taking actions.
+                       This tensor contains 1 trajectories of T length.
+        """
+        state_1 = self._dynamics(state_0, actions[0])
+        trajectory = state_1.unsqueeze(1)
+        for t in range(1, self.T):
+          state = self._dynamics(state_1, actions[t])
+          trajectory = torch.hstack((trajectory, state.unsqueeze(1)))
+          state_1 = state
+        return trajectory
+
+    def _backward_pass(self):
+        V_0 = torch.zeros(self.T+1)
+        V_x = torch.zeros((self.state_size, self.T+1))
+        V_xx = torch.zeros((self.state_size, self.state_size, self.T+1))
+
+        for i in reversed(range(self.T-1)):
+            mu_1 = 0
+            mu_2 = 0
+            f_x, f_u, f_ux, f_xx, f_uu = self.find_f_gredient(self.X[i], self.U[i])
+            L_x, L_u, L_xx, L_xu, L_uu = self.find_cost_gredient(self.X[i], self.U[i])
+            Q_x = L_x + f_x.T @ V_x[i+1]
+            Q_u = L_u + f_u.T @ V_x[i+1]
+            Q_xx = L_xx + f_x.T @ V_xx[i+1] @ f_x + V_x[i+1] @ f_xx
+            Q_ux = L_xu + f_u.T @ V_xx[i+1] @ f_x + V_x[i+1] @ f_ux
+            Q_uu = L_uu + f_u.T @ V_xx[i+1] @ f_u + V_x[i+1] @ f_uu
+            inv_Q_uu = None
+            while inv_Q_uu is None:
+                try:
+                    inv_Q_uu = torch.linalg.inv(Q_uu)
+                except:
+                    mu_1 += 0.1
+                    mu_2 += 0.1
+            self.k[i] = -torch.linalg.inv(Q_uu) @ Q_u
+            self.K[i] = -torch.linalg.inv(Q_uu) @ Q_ux
+
+            V_0[i] = V_0[i+1] - Q_u.T @ inv_Q_uu @ Q_u /2 # +L_0
+            V_x[i] = Q_x - Q_ux.T @ inv_Q_uu @ Q_u
+            V_xx[i] = Q_xx - Q_ux.T @ inv_Q_uu @ Q_ux
+
+    def _foward_pass(self):
+        x = self.X
+        dx = torch.zeros_like(self.X)
+        eps = 1
+        for i in range(self.T-1):
+            dx[i] = x[i] - self.X[i]
+            self.U[i] += eps*self.k[i] + self.K[i] @ dx[i]
+            x[i+1] = self._dynamics(x[i], self.U[i])
+
+    def _compute_trajectory_cost(self, trajectory):
+        """
+        Compute the costs for the K different trajectories
+        :param trajectory: torch tensor of shape (T, state_size)
+        :param perturbations: torch tensor of shape (T, action_size)
+        :return:
+         - total_trajectory_cost: torch tensor of shape (1,) containing the total trajectory costs for the K trajectories
+        Observations:
+        * The trajectory cost be the sum of the state costs and action costs along the trajectories
+        * State cost should be quadratic as (state_i-goal_state)^T Q (state_i-goal_state)
+        * Action costs "TODO"
+        """
+        total_trajectory_cost = None
+        # --- Your code here
+        dist2goal = trajectory[:,None,:]-self.goal_state.reshape(1,1,-1)
+        total_trajectory_cost = torch.matmul(torch.matmul(dist2goal, self.Q), dist2goal.permute(0,2,1)).squeeze()
+        total_trajectory_cost = torch.sum(total_trajectory_cost, dim=1)
+        # ---
+        return total_trajectory_cost
+
+    def _dynamics(self, state, action):
+        """
+        Query the environment dynamics to obtain the next_state in a batched format.
+        :param state: torch tensor of size (...., state_size)
+        :param action: torch tensor of size (..., action_size)
+        :return: next_state: torch tensor of size (..., state_size)
+        """
+        next_state = self.env.dynamics(state.cpu().detach().numpy(), action.cpu().detach().numpy())
+        next_state = torch.tensor(next_state, dtype=state.dtype)
+        return next_state
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from IPython.display import Image
 from tqdm import tqdm
 
 from cartpole_env import *
-from dynamics_analytic import *
+from dynamics import *
 from mppi_control import *
+from ddp_control import *
 
 def main():
     # env = CartpoleEnv()
@@ -25,64 +26,91 @@ def main():
     # write_apng("cartpole_example.png", frames, delay=10)
     # Image(filename="cartpole_example.png")
 
+    # Linearization around 
+    env = CartpoleEnv()
+    # B = 4
+    # states = 0.1 * torch.randn(B, 6)
+    # actions = torch.randn(B, 1)
+
+    # # first lets see what the pybullet dynamics are
+    # print('Next states from simulator are: ')
+    # for state, action in zip(states, actions):
+    #     print(env.dynamics(state.numpy(), action.numpy()))
+
+    # print('')
+    # print('Batched next states from analytical dynamics are')
+    # print(dynamics_analytic(states, actions))
 
 
-    ## Let's test to see if your analytic dynamics matches the simulator
+    A_numerical, B_numerical = env.linearize_numerical(np.zeros(6), np.zeros(1))
+
+    print('Numerical Linearizations are ')
+    print(A_numerical)
+    print(B_numerical)
+    print('')
+    A_autograd, B_autograd = linearize_pytorch(torch.zeros(6), torch.zeros(1))
+
+    print('Autograd linearizations are ')
+    print(A_autograd)
+    print(B_autograd)
+
+
+    # Let's test to see if your analytic dynamics matches the simulator
 
     # first let's generate a random control sequence
-    # T = 50
-    # control_sequence = np.random.randn(T, 1)
-    # start_state = np.array([0.0, 0.25, 0.25, 0.0, 0.0, 0.0])
+    T = 50
+    control_sequence = np.random.randn(T, 1)
+    start_state = np.array([0.0, 0.5, 0.5, 0.0, 0.0, 0.0])
 
-    # # We use the simulator to simulate a trajectory
-    # env = CartpoleEnv()
-    # env.reset(start_state)
-    # states_pybullet = np.zeros((T+1, 6))
-    # states_pybullet[0] = start_state
-    # for t in range(T):
-    #     states_pybullet[t+1] = env.step(control_sequence[t])
+    # We use the simulator to simulate a trajectory
+    env = CartpoleEnv()
+    env.reset(start_state)
+    states_pybullet = np.zeros((T+1, 6))
+    states_pybullet[0] = start_state
+    for t in range(T):
+        states_pybullet[t+1] = env.step(control_sequence[t])
 
-    # # Now we will use your analytic dynamics to simulate a trajectory
-    # states_analytic = torch.zeros(T+1, 1, 6) # Need an extra 1 which is the batch dimension (T x B x 4)
-    # states_analytic[0] = torch.from_numpy(start_state).reshape(1, 6)
-    # for t in range(T):
-    #     current_state = states_analytic[t]
-    #     current_control = torch.from_numpy(control_sequence[t]).reshape(1, 1) # add batch dimension to control    
-    #     states_analytic[t+1] = dynamics_analytic(current_state, current_control)
+    # Now we will use your analytic dynamics to simulate a trajectory
+    states_analytic = torch.zeros(T+1, 1, 6) # Need an extra 1 which is the batch dimension (T x B x 4)
+    states_analytic[0] = torch.from_numpy(start_state).reshape(1, 6)
+    for t in range(T):
+        current_state = states_analytic[t]
+        current_control = torch.from_numpy(control_sequence[t]).reshape(1, 1) # add batch dimension to control    
+        states_analytic[t+1] = dynamics_analytic(current_state, current_control)
         
-    # # convert back to numpy for plotting
-    # states_analytic = states_analytic.reshape(T+1, 6).numpy()
+    # convert back to numpy for plotting
+    states_analytic = states_analytic.reshape(T+1, 6).numpy()
 
-    # # Plot and compare - They should be indistinguishable 
-    # fig, axes = plt.subplots(2, 3, figsize=(8, 8))
-    # axes[0][0].plot(states_analytic[:, 0], label='analytic')
-    # axes[0][0].plot(states_pybullet[:, 0], '--', label='pybullet')
-    # axes[0][0].title.set_text('x')
+    # Plot and compare - They should be indistinguishable 
+    fig, axes = plt.subplots(2, 3, figsize=(8, 8))
+    axes[0][0].plot(states_analytic[:, 0], label='analytic')
+    axes[0][0].plot(states_pybullet[:, 0], '--', label='pybullet')
+    axes[0][0].title.set_text('x')
 
-    # axes[0][1].plot(states_analytic[:, 1])
-    # axes[0][1].plot(states_pybullet[:, 1], '--')
-    # axes[0][1].title.set_text('theta_1')
+    axes[0][1].plot(states_analytic[:, 1])
+    axes[0][1].plot(states_pybullet[:, 1], '--')
+    axes[0][1].title.set_text('theta_1')
 
-    # axes[1][0].plot(states_analytic[:, 3])
-    # axes[1][0].plot(states_pybullet[:, 3], '--')
-    # axes[1][0].title.set_text('x_dot')
+    axes[1][0].plot(states_analytic[:, 3])
+    axes[1][0].plot(states_pybullet[:, 3], '--')
+    axes[1][0].title.set_text('x_dot')
 
-    # axes[1][1].plot(states_analytic[:, 4])
-    # axes[1][1].plot(states_pybullet[:, 4], '--')
-    # axes[1][1].title.set_text('theta_1_dot')
+    axes[1][1].plot(states_analytic[:, 4])
+    axes[1][1].plot(states_pybullet[:, 4], '--')
+    axes[1][1].title.set_text('theta_1_dot')
 
-    # axes[0][2].plot(states_analytic[:, 2])
-    # axes[0][2].plot(states_pybullet[:, 2], '--')
-    # axes[0][2].title.set_text('theta_2')
+    axes[0][2].plot(states_analytic[:, 2])
+    axes[0][2].plot(states_pybullet[:, 2], '--')
+    axes[0][2].title.set_text('theta_2')
 
-    # axes[1][2].plot(states_analytic[:, 5])
-    # axes[1][2].plot(states_pybullet[:, 5], '--')
-    # axes[1][2].title.set_text('theta_2_dot')
+    axes[1][2].plot(states_analytic[:, 5])
+    axes[1][2].plot(states_pybullet[:, 5], '--')
+    axes[1][2].title.set_text('theta_2_dot')
 
-    # axes[0][0].legend()
-    # plt.show()
+    axes[0][0].legend()
+    plt.show()
 
-
+    # MPPI
     env = CartpoleEnv()
     env.reset(np.array([0, np.pi, 0, 0, 0, 0]) + np.random.rand(6,)) 
     goal_state = np.zeros(6)
@@ -98,7 +126,6 @@ def main():
         s = env.step(control) 
         error_i = np.linalg.norm(s-goal_state[:7])
         pbar.set_description(f'Goal Error: {error_i:.4f}')
-        pbar.refresh()
         img = env.render()
         frames.append(img)
         if error_i < .1:
@@ -106,6 +133,32 @@ def main():
     print("creating animated gif, please wait about 10 seconds")
     write_apng("cartpole_mppi.gif", frames, delay=10)
     Image(filename="cartpole_mppi.gif")
+
+    # DDP
+    # Define problem parameters
+    env = CartpoleEnv()
+    env.reset(np.array([0, np.pi, 0, 0, 0, 0]) + np.random.rand(6,)) 
+    goal_state = np.zeros(6)
+    controller = DDPController(env, horizon=30)
+    controller.goal_state = torch.tensor(goal_state, dtype=torch.float32)
+    frames = []
+    num_steps = 70
+    pbar = tqdm(range(num_steps))
+    for i in pbar:
+        state = env.get_state()
+        state = torch.tensor(state, dtype=torch.float32)
+        control = controller.command(state)
+        s = env.step(control) 
+        error_i = np.linalg.norm(s-goal_state[:7])
+        pbar.set_description(f'Goal Error: {error_i:.4f}')
+        img = env.render()
+        frames.append(img)
+        if error_i < .1:
+            break
+    
+    print("creating animated gif, please wait about 10 seconds")
+    write_apng("cartpole_ddp.gif", frames, delay=10)
+    Image(filename="cartpole_ddp.gif")
 
 
 if __name__ == '__main__':
